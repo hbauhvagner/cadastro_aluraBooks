b476d6104ad8e580e6e2c04e3291478e4a1694ce
hbauhvagner <hbauhvagner@gmail.com>
2023-07-06 00:07:35 -0300

    Tratando erros com catch

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
     .then(resposta => resposta.json())
-    .then(r => console.log(r));
+    .then(r => {
+        if (r.erro) {
+            throw Error('Esse cep não existe');
+        } else {
+            console.log(r)
+        }
+    })
+    .catch(erro => console.log(erro));
