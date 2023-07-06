725c69d75c7fdac53de132797c494a4099ccabec
hbauhvagner <hbauhvagner@gmail.com>
2023-07-05 23:57:23 -0300

    Then e JSON

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var consultaCEP = fetch('viacep.com.br/ws/01001000/json');
-
-console.log(consultaCEP);
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
+    .then(resposta => resposta.json())
+    .then(r => console.log(r));
