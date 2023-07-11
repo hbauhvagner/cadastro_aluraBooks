5a3fec3dfdd34686da051ddb0dedf8621db98470
hbauhvagner <hbauhvagner@gmail.com>
2023-07-10 22:57:15 -0300

    Async Await

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe');
-        } else {
-            console.log(r)
-        }
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+// var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
+//     .then(resposta => resposta.json())
+//     .then(r => {
+//         if (r.erro) {
+//             throw Error('Esse cep não existe');
+//         } else {
+//             console.log(r)
+//         }
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('Processamento concluído!'));
+
+async function buscaEndereco() {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json');
+    var consultaCEPConvertida = await consultaCEP.json();
+
+    console.log(consultaCEPConvertida);
+}
+
+buscaEndereco();
